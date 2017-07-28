8c432d21a08e0e45a0c4dfe39d7512fa2a1aa309
Collin Cromwell <collcrom@gmail.com>
2017-07-28 17:45:44 -0400

    created some game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,2 +1,14 @@
-var card = ["queen", "queen", "king", "king"];
-console.log("User flipped " + card[0] + " and " + card[3]);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+if(cardsInPlay.length === 2)
+{
+	if(cardsInPlay[0] === cardsInPlay[1])
+		alert("You found a match!");
+	else
+		alert("Sorry try again.");
+}
